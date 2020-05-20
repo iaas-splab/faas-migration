488fb6225f9f1d472fe8676a2cf9652b34447672
Vladimir Yussupov <v.yussupov@gmail.com>
2020-05-20 21:09:15 +0200

    Refactor thumbnail generation handler
    Signed-off-by: Vladimir Yussupov <v.yussupov@gmail.com>

--- ThumbnailGenerator/Lambda/src/main/java/spblab/thumbgen/lambda/ThumbnailGenerationHandler.java
+++ ThumbnailGenerator/Lambda/src/main/java/spblab/thumbgen/lambda/ThumbnailGenerationHandler.java
@@ -1,62 +1,74 @@
 package spblab.thumbgen.lambda;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.event.S3EventNotification;
+import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.util.IOUtils;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import static spblab.thumbgen.lambda.Config.THUMBNAIL_BUCKET;
 
-@SuppressWarnings("unused")
-public class ThumbnailGenerationHandler implements RequestHandler<S3Event, Void> {
+public class ThumbnailGenerationHandler implements RequestHandler<S3Event, String> {
     private static final Logger LOG = LogManager.getLogger(ThumbnailGenerationHandler.class);
 
-    private ObjectMapper mapper = new ObjectMapper();
-    private AmazonS3Client client = new AmazonS3Client();
-
     @Override
-    public Void handleRequest(S3Event input, Context context) {
+    public String handleRequest(S3Event input, Context context) {
         StringBuilder bout = new StringBuilder();
 
-        String resultMessage = null;
+        S3EventNotification.S3EventNotificationRecord record = input.getRecords().get(0);
 
-        for (S3EventNotification.S3EventNotificationRecord record : input.getRecords()) {
-            LOG.info("Loading {}/{}", record.getS3().getBucket().getName(), record.getS3().getObject().getKey());
-            InputStream in = null;
-            try {
-                S3Object obj = client.getObject(record.getS3().getBucket().getName(), record.getS3().getObject().getKey());
-                in = obj.getObjectContent();
+        String srcBucket = record.getS3().getBucket().getName();
+        String srcKey = record.getS3().getObject().getUrlDecodedKey();
+        String dstKey = "resized-" + srcKey;
+        LOG.info("Loading {}/{}", srcBucket, srcKey);
 
-                byte[] convertedImage = Converter.createThumbnail(IOUtils.toByteArray(in));
-
-                ByteArrayInputStream tin = new ByteArrayInputStream(convertedImage);
-
-                PutObjectResult response = client.putObject(THUMBNAIL_BUCKET, record.getS3().getObject().getKey(), tin, new ObjectMetadata());
-
-                bout.append("Processed Image: `").append(record.getS3().getObject().getKey()).append("`\n");
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    in.close();
-                } catch (IOException e) {
-
-                }
-            }
-            LOG.info("Done!");
+        Converter c = new Converter(srcKey);
+        if (!c.isTypeInferable()) {
+            LOG.info("Unable to infer image type for key " + srcKey);
+            return "No thumbnail for you!";
         }
-        return null;
+        if (!c.isImage()) {
+            LOG.info("Skipping non-image " + srcKey);
+            return "No thumbnail for you!";
+        }
+
+        try {
+            LOG.info("Starting thumbnail generation process");
+            AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+            S3Object s3Object = s3Client.getObject(new GetObjectRequest(srcBucket, srcKey));
+            InputStream objectData = s3Object.getObjectContent();
+            ByteArrayOutputStream os = c.createThumbnail(objectData);
+
+            InputStream convertedImage = new ByteArrayInputStream(os.toByteArray());
+            // Set Content-Length and Content-Type
+            ObjectMetadata meta = new ObjectMetadata();
+            meta.setContentLength(os.size());
+            meta.setContentType(c.getImageMimeType());
+
+            LOG.info("Writing the thumbnail to: " + THUMBNAIL_BUCKET + "/" + dstKey);
+            s3Client.putObject(THUMBNAIL_BUCKET, dstKey, convertedImage, meta);
+
+            LOG.info("Successfully resized " + srcBucket + "/" + srcKey + " and uploaded to " + THUMBNAIL_BUCKET + "/" + dstKey);
+            return "Ok";
+        } catch (IOException | AmazonServiceException e) {
+            e.printStackTrace();
+            LOG.error("Thumbnail generation failed: {}", e.getMessage());
+            System.exit(1);
+        }
+        LOG.info("Thumbnail generation is finished!");
+
+        return "Done!";
     }
 }
